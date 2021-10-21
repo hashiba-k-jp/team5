03da7d4722932c1616010ca9255e019b0e9a69c2
himeji17142 <17142@hakodate.kosen-ac.jp>
2021-10-21 11:58:57 +0900

    Bがキャラ移動処理を追加

--- game5/HS.java
+++ game5/HS.java
@@ -14,6 +14,9 @@ public class HS extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+        }// Add your action code here.
     }    
 }
