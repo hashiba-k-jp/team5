5028969d34d225c037d532e1c9d002d664ed42b3
himeji17142 <17142@hakodate.kosen-ac.jp>
2021-10-21 11:28:46 +0900

    Bがキャラ追加

---
+++ game5/s.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class s here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class s extends Actor
+{
+    /**
+     * Act - do whatever the s wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
