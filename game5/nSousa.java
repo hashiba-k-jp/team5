e8e4e7ca45da24e177ff81dadc1438c5ab225bac
Nozawa-Masak <17130@hakodate.kosen-ac.jp>
2021-11-09 12:05:02 +0900

    Cがキャラ移動処理を追加

--- game5/nSousa.java
+++ game5/nSousa.java
@@ -14,6 +14,23 @@ public class nSousa extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+      if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(-90);
+        move(1);
+        setRotation(0);
+      }
+      if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(1);
+        setRotation(0);
+      }
+      if( Greenfoot.isKeyDown( "left" ) ){
+        setRotation(0);
+        move(-1);
+      }
+      if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+      }
     }    
 }
