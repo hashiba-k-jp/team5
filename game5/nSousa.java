4389aa215e01fea0abe37a393a0d7c2105c68cd0
hirai1228 <17143@hakodate.kosen-ac.jp>
2021-11-18 10:55:08 +0900

    AがnSousaを追加

--- game5/nSousa.java
+++ game5/nSousa.java
@@ -6,6 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class nSousa extends Actor
 {
     /**
@@ -14,6 +15,6 @@ public class nSousa extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        getImage().scale( 75, 75 );// Add your action code here.
     }    
 }
