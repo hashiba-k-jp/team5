ad9b023f6781693dfeee7b1574052d4c49245399
Nozawa-Masak <17130@hakodate.kosen-ac.jp>
2021-11-18 11:00:47 +0900

    Cがキャラ操作追加

--- game5/nSousa.java
+++ game5/nSousa.java
@@ -12,6 +12,10 @@ public class nSousa extends Actor
      * Act - do whatever the nSousa wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public nSousa(){
+        getImage().scale( 75, 75 );
+    }
+    
     public void act() 
     {
       if( Greenfoot.isKeyDown( "up" ) ){
