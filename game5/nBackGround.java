28943ca7de877d64453ec01a20efd7c1fdc3043b
hirai1228 <17143@hakodate.kosen-ac.jp>
2021-12-02 11:34:58 +0900

    Aがタイマーを追加

--- game5/nBackGround.java
+++ game5/nBackGround.java
@@ -16,10 +16,17 @@ public class nBackGround extends World
     public nBackGround()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        
         super(600, 400, 1); 
-
         Actor SButton = new startButton();
         addObject(SButton, 300, 200);
+    }
+    public void act(){
+        timecount=timecount-1;
+        showText( "timecount = " + timecount , 100, 50 );
+        if(timecount <= 0)
+        {
+          addObject( new gameOver(), 300, 200 );
+          Greenfoot.stop();  
+        }
     }
 }
