8d96908685718c3f11d86292cf07e21fed118eac
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2022-01-13 11:47:25 +0900

    add missile

--- game5/hmissile.java
+++ game5/hmissile.java
@@ -12,9 +12,14 @@ public class hmissile extends Actor
      * Act - do whatever the hmissile wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public hmissile(){
+        this.setRotation(0);
+        getImage().scale(60, 30);
+
+    }
     public void act()
     {
-        this.setRotation(-90);
+
         // Add your action code here.
     }
 }
