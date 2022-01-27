56fe365635ffa6b5275d3dcf8d29b6ee6c348015
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2022-01-27 11:24:17 +0900

    add missile-chaised

--- game5/hmissile.java
+++ game5/hmissile.java
@@ -12,15 +12,31 @@ public class hmissile extends Actor
      * Act - do whatever the hmissile wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-
-    public hmissile(){
+    int i = 0;
+    int xOrigin, yOrigin, xTarget, yTarget;
+    
+    // i := input
+    public hmissile(int xTargeti, int yTargeti, int xOrigini, int yOrigini){
         this.setRotation(-180);
         getImage().scale(30, 15);
+        xOrigin = xOrigini;
+        yOrigin = yOrigini;
+        xTarget = xTargeti;
+        yTarget = yTargeti;
+        
     }
-    public void act(int xTargetF)
+    // Origin, Target
+    public void act()
     {
-        if (xTargetF == 0){
-        }
+        double x, y;
+        x = xOrigin + (xTarget - xOrigin) * i / 100;
+        y = yOrigin + (yTarget - yOrigin) * i / 100;
+        // move(10);
+        this.setLocation((int)x, (int)y);
         // Add your action code here.
+        i++;
+        if(100 <= i){
+            getWorld().removeObject( this );
+        }
     }
 }
