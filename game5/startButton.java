6677dbbd2e58da4e5afaea103dfc0a1de45fa5da
hirai1228 <17143@hakodate.kosen-ac.jp>
2022-01-27 11:22:11 +0900

    Aがタイトルテキストを追加

--- game5/startButton.java
+++ game5/startButton.java
@@ -13,7 +13,7 @@ public class startButton extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     private GreenfootImage img_bkup = null;
-    private int width=50;
+    private int width= 50;
     private int height= 50;
 
     public startButton(){
